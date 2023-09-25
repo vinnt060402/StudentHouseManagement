bdee585d66f9ddae4d78ad34200a229b5dd42782
vinnt060402 <vinnt060402@gmail.com>
2023-09-25 08:21:02 +0700

    Create Entity Apartment, Region and Student

---
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Domain/Entities/Student.cs
@@ -0,0 +1,14 @@
+﻿using StudentHouseMembershipCart.Domain.Common;
+
+namespace StudentHouseMembershipCart.Domain.Entities
+{
+    public class Student : BaseAuditableEntity  
+    {
+        public string StudentName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+    }
+}
