2239f2611542ee41aeda8d537630e5959a2a47f4
vinnt060402 <vinnt060402@gmail.com>
2023-09-27 22:14:58 +0700

    Add Global Exception

---
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace StudentHouseMembershipCart.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base()
+        {
+
+        }
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message)
+        {
+
+        }
+
+        public NotFoundException(string name, object key) : base($"Enity \"{name}\" ({key}) was not found.")
+        {
+
+        }
+
+        public NotFoundException(string name, object key, string message) : base(JsonSerializer.Serialize(new
+        {
+            DebugMessage = $"Enity {name} with Id: ({key}) was not found.",
+            Message = message
+        }, new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping }))
+        {
+
+        }
+    }
+}
