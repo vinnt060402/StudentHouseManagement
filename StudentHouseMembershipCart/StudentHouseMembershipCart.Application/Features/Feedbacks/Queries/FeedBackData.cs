b022e31eaba614c6a4c645c8b89660b87fc19e9c
vinnt060402 <vinnt060402@gmail.com>
2023-11-15 21:28:29 +0700

    Commit Get Feedback

---
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Feedbacks/Queries/FeedBackData.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentHouseMembershipCart.Application.Features.Feedbacks.Queries
+{
+    public class FeedBackData
+    {
+        public string? FeedBackName { get; set; }
+        public string? FeedBackDescription { get; set; }
+        public string? FeedBackImage { get; set; }
+        public string? FeedBackRating { get; set; }
+        /// <summary>
+        /// 0. Can not Feedback
+        /// 1. Can Feedback
+        /// 2. Feedbacked
+        /// </summary>
+        public int FeedBackStatus { get; set; }
+    }
+}
---
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Feedbacks/Queries/GetListFeedBackByBookingDetailId/GetListFeedBackByBookingDetailIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace StudentHouseMembershipCart.Application.Features.Feedbacks.Queries.GetListFeedBackByBookingDetailId
+{
+    public class GetListFeedBackByBookingDetailIdQuery : IRequest<List<FeedBackData>>
+    {
+        public string BookingDetailId { get; set; } = null!;
+    }
+}
