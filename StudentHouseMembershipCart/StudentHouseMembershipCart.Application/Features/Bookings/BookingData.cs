9388f3ecc160afce5afe542c2adda07230b7249b
vinnt060402 <vinnt060402@gmail.com>
2023-11-16 01:46:51 +0700

    Add Features Booking For Presentation

--- StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Bookings/BookingData.cs
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Bookings/BookingData.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using StudentHouseMembershipCart.Application.Features.Apartments;
+using StudentHouseMembershipCart.Application.Features.BookingDetails.Queries;
+
+namespace StudentHouseMembershipCart.Application.Features.Bookings
+{
+    public class BookingData
+    {
+        public Guid Id { get; set; }
+        public string? StudentName { get; set; }
+        public string? PaymentMethodName { get; set; }
+        public string? Contract { get; set; }
+        public double TotalPay { get; set; }
+        public Guid ApartmentId { get; set; }
+        public ApartmentDto? ApartmentData { get; set; }
+        /// <summary>
+        /// If StatusContract is 0 -> On going
+        /// Is 1 -> Finished
+        /// </summary>
+        public string? StatusContract { get; set; }
+        public DateTime Created { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<BookingDetailData>? Details { get; set; }
+    }
+}
