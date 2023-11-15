58f260027127dba1db4fd511d2a7cc438c3d7b22
Nguyễn Phạm Khanh <khanhnpse160945@fpt.edu.vn>
2023-11-16 02:38:30 +0700

    submit regions feature

--- StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Regions/Queries/GetAllRegion/GetLisRegionQueryHandler.cs
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Regions/Queries/GetAllRegion/GetLisRegionQueryHandler.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StudentHouseMembershipCart.Application.Common.Exceptions;
+using StudentHouseMembershipCart.Application.Common.Interfaces;
+using StudentHouseMembershipCart.Domain.Entities;
+using StudentHouseMembershipCart.Domain.IdentityModels;
+
+namespace StudentHouseMembershipCart.Application.Features.Regions.Queries.GetAllRegion
+{
+    public class GetLisRegionQueryHandler : IRequestHandler<GetListRegionQuery, List<RegionDto>>
+    {
+        private IApplicationDbContext _context { get; set; }
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IMapper _mapper;
+
+        public GetLisRegionQueryHandler(IApplicationDbContext context, IMapper mapper, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _mapper = mapper;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<List<RegionDto>> Handle(GetListRegionQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _context.Region.Where(r => r.IsDelete == false)
+                .ProjectTo<RegionDto>(_mapper.ConfigurationProvider).ToListAsync();
+            if (list == null)
+            {
+                throw new NotFoundException(nameof(Region));
+            }
+
+            return list;
+        }
+    }
+}
--- StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Regions/Queries/GetAllRegion/GetListRegionQuery.cs
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/Regions/Queries/GetAllRegion/GetListRegionQuery.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHouseMembershipCart.Application.Features.Regions.Queries.GetAllRegion
+{
+    public class GetListRegionQuery : IRequest<List<RegionDto>>
+    {
+    }
+}
