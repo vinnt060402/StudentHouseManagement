cf42a07b548a33f9ca39ec6c338a3df7d359a1e8
vinnt060402 <vinnt060402@gmail.com>
2023-11-25 03:22:39 +0700

    Commit Get AttenReport

--- StudentHouseMembershipCart/StudentHouseMembershipCart.API/Controllers/AttendReports/AttendReportsController.cs
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.API/Controllers/AttendReports/AttendReportsController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using StudentHouseMembershipCart.Application.Common.Response;
 using StudentHouseMembershipCart.Application.Features.AttendenceReports.Commands.CreateAttendenceReport;
+using StudentHouseMembershipCart.Application.Features.AttendenceReports.Queries.GetAttendReportByServiceId;
 using StudentHouseMembershipCart.Application.Features.Bookings.Commands.CreateBooking;
+using StudentHouseMembershipCart.Application.Features.Bookings.QueriesNew.GetAllBookingDoNotAssignBefore;
 
 namespace StudentHouseMembershipCart.API.Controllers.AttendReports
 {
@@ -19,6 +21,12 @@ namespace StudentHouseMembershipCart.API.Controllers.AttendReports
 
         [HttpPost]
         public async Task<SHMResponse> CreateNewAttendData(CreateAttendenceReportCommand request)
+        {
+            return await _mediator.Send(request);
+        }
+        [HttpGet]
+        [Route("get-attendreport-by-student-id-and-service-id")]
+        public async Task<List<NewAttendReportByService>> GetAllBookingDetailDoNotAssignBefore([FromQuery]GetAttendReportByServiceIdQuery request)
         {
             return await _mediator.Send(request);
         }
