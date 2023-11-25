617ddb3f0826657d685cb56ddc5b6883b1964f5f
vinnt060402 <vinnt060402@gmail.com>
2023-11-26 01:40:36 +0700

    Create Feedback

--- StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/AttendenceReports/Commands/CreateAttendenceReport/CreateAttendenceReportCommandHandler.cs
+++ StudentHouseMembershipCart/StudentHouseMembershipCart.Application/Features/AttendenceReports/Commands/CreateAttendenceReport/CreateAttendenceReportCommandHandler.cs
@@ -26,13 +26,13 @@ namespace StudentHouseMembershipCart.Application.Features.AttendenceReports.Comm
                                                              x.AttendReportForType == request.BookingDetailType).ToListAsync();
             var listCheckInt = new List<int>();
             int numFlag = 0;
-            if(aRCheck != null)
+            if (aRCheck != null)
             {
                 var titleNumber = aRCheck.Select(x => x.AttendTittle).ToList();
-                foreach(var item in titleNumber)
+                foreach (var item in titleNumber)
                 {
                     var getlast3character = item!.Substring(item.Length - 3);
-                    if(getlast3character != null)
+                    if (getlast3character != null)
                     {
                         int number = int.Parse(getlast3character.Split('(')[1].Split(')')[0]);
                         listCheckInt.Add(number);
@@ -74,7 +74,7 @@ namespace StudentHouseMembershipCart.Application.Features.AttendenceReports.Comm
             var serviceTaskRemain = await _dbContext.ServiceRemainingTaskDuration.Where(x => x.BookingDetailId == Guid.Parse(request.BookingDetailId) &&
                                                                                              x.ServiceId == Guid.Parse(request.ServiceId) &&
                                                                                              x.AttendReportForType == request.BookingDetailType).SingleOrDefaultAsync();
-            if(serviceTaskRemain == null)
+            if (serviceTaskRemain == null)
             {
                 throw new BadRequestException("Remaining Task Duration Is Not Found So can not create Attend for you!!");
             }
@@ -96,6 +96,35 @@ namespace StudentHouseMembershipCart.Application.Features.AttendenceReports.Comm
             {
                 throw new BadRequestException("Service date cannot be less than the end date");
             }
+            Guid StudentId;
+            if (request.BookingDetailType == "1")
+            {
+                StudentId = await (from bdP in _dbContext.BookingDetailOfPakcage
+
+                                   join b in _dbContext.Booking
+
+                                   on bdP.BookingId equals b.Id
+
+                                   join a in _dbContext.Apartment
+
+                                   on b.ApartmentId equals a.Id
+
+                                   select a.StudentId).FirstOrDefaultAsync();
+            }
+            else
+            {
+                StudentId = await (from bdS in _dbContext.BookingDetailOfService
+
+                                   join b in _dbContext.Booking
+
+                                   on bdS.BookingId equals b.Id
+
+                                   join a in _dbContext.Apartment
+
+                                   on b.ApartmentId equals a.Id
+
+                                   select a.StudentId).FirstOrDefaultAsync();
+            }
             /*
              * Handle chu kỳ làm việc
              * Chu kỳ là việc chúng ta sẽ thực hiện Service đó trong khoản thời gian chu kỳ
@@ -104,13 +133,13 @@ namespace StudentHouseMembershipCart.Application.Features.AttendenceReports.Comm
              * ( thứ 6 tuần 1 - thứ 6 tuần 2 - thứ 6 tuần 3 - thứ 6 tuần 4)
              * Tương tự cho các loại chu kỳ còn lại
             */
-            var listDateDoService = HandleCreateListTimeDoService(request.DateDoService, EndDateCheck, request.WorkingCycle, request.FrequencyDaysPerOccurrence,request.QuantityDoService,serviceTaskRemain.RemainingTaskDuration);
+            var listDateDoService = HandleCreateListTimeDoService(request.DateDoService, EndDateCheck, request.WorkingCycle, request.FrequencyDaysPerOccurrence, request.QuantityDoService, serviceTaskRemain.RemainingTaskDuration);
             if (serviceTaskRemain != null)
             {
-                serviceTaskRemain.RemainingTaskDuration = serviceTaskRemain.RemainingTaskDuration - listDateDoService.Count*request.QuantityDoService;
+                serviceTaskRemain.RemainingTaskDuration = serviceTaskRemain.RemainingTaskDuration - listDateDoService.Count * request.QuantityDoService;
                 _dbContext.ServiceRemainingTaskDuration.Update(serviceTaskRemain);
             }
-            if(numFlag == 0)
+            if (numFlag == 0)
             {
                 numFlag = 1;
             }
@@ -123,16 +152,24 @@ namespace StudentHouseMembershipCart.Application.Features.AttendenceReports.Comm
                 //Tạo AttendReport
                 var aR = new AttendReport()
                 {
-                    AttendTittle = "Work for Service " + service!.ServiceName!.Trim() + ", Date: " + item.ToString("yyyy/MM/dd HH:mm") + " "+"("+numFlag.ToString()+")",
+                    AttendTittle = "Work for Service " + service!.ServiceName!.Trim() + ", Date: " + item.ToString("yyyy/MM/dd HH:mm") + " " + "(" + numFlag.ToString() + ")",
                     DateDoService = item,
                     ServiceId = Guid.Parse(request.ServiceId),
                     BookingDetailId = Guid.Parse(request.BookingDetailId),
                     AttendReportForType = request.BookingDetailType,
                     AttendenceStatus = 0,
-                    Note = request.Note + " Quantity to do: "+request.QuantityDoService,
+                    Note = request.Note + " Quantity to do: " + request.QuantityDoService,
                 };
+
                 _dbContext.AttendReport.Add(aR);
 
+                var feedback = new FeedBack()
+                {
+                    FeedBackStatus = 0,
+                    StudentId = StudentId,
+                    AttendReportId = aR.Id
+                };
+                _dbContext.FeedBack.Add(feedback);
                 //TODO Tạo Handle tạo Task cho nhân viên -> từ Task sẽ có ReportWork + Feedback
             }
 
@@ -154,7 +191,7 @@ namespace StudentHouseMembershipCart.Application.Features.AttendenceReports.Comm
                 {
                     list.Add(DateDoService);
                     flagCheckQuantityAfterDo = flagCheckQuantityAfterDo - quantityDoService;
-                    if(flagCheckQuantityAfterDo < 0)
+                    if (flagCheckQuantityAfterDo < 0)
                     {
                         throw new BadRequestException("The remaining amount of service is not enough");
                     }
